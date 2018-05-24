62a6e9e8c0c2e4b1ca1ab39c768d45c831837088
Dandu, Praveen Kumar Reddy <Praveen.Dandu@priceline.com>
2018-05-24 06:57:46 -0400

    created linked list classes

---
+++ src/praveen/ds/LinkedListImpl.java
@@ -0,0 +1,14 @@
+package praveen.ds;
+
+public class LinkedListImpl {
+
+	public LinkedListImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/praveen/ds/LinkedListNode.java
@@ -0,0 +1,14 @@
+package praveen.ds;
+
+public class LinkedListNode {
+
+	public LinkedListNode() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
